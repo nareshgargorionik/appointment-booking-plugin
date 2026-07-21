98a6aa349aad9276a9b5f172a53468e5ed0aaab1
jaydeep-gadhavi <jaydeep.gadhavi@vitreousco.com>
2026-07-21 16:48:03 +0530

    style changes.

--- src/components/steps/SuccessPage.tsx
+++ src/components/steps/SuccessPage.tsx
@@ -166,7 +166,7 @@ export default function SuccessPage(): JSX.Element {
                                         {s.serviceName}
                                     </span>
 
-                                    <span className="aaravpos-service-price">
+                                    <span className="aaravpos-value">
                                         <CurrencyIcon size={14} />
                                         {Number(s.price).toFixed(2)}
                                     </span>
