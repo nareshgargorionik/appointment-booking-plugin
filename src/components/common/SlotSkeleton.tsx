04c071eacbe7e220fa3153411073300c1f95f881
Hardik Dongre <hardik.dongre@vitreousco.com>
2026-08-21 17:01:53 +0530

    update ui

--- src/components/common/SlotSkeleton.tsx
+++ src/components/common/SlotSkeleton.tsx
@@ -2,14 +2,13 @@ import type { JSX } from "react";
 
 export default function SlotSkeleton(): JSX.Element {
   return (
-    <div className="aaravpos-barber-scroll-area">
+    <div className="aaravpos-scroll-area">
       {Array.from({ length: 3 }).map((_, sectionIndex) => (
-        <div key={sectionIndex} className="aaravpos-barber-slot-section">
+        <div key={sectionIndex} className="aaravpos-slot-section">
           {/* HEADER */}
-          <div className="aaravpos-barber-slot-section-header">
-            <div className="aaravpos-barber-slot-skeleton-title shimmer" />
-
-            <div className="aaravpos-barber-slot-skeleton-arrow shimmer" />
+          <div className="aaravpos-slot-section-header">
+            <div className="aaravpos-slot-skeleton-title shimmer" />
+            <div className="aaravpos-slot-skeleton-arrow shimmer" />
           </div>
         </div>
       ))}
